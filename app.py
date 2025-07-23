8423d699c0acde5baa934afee28db6c307014462
Gabreu <gabreumartins02@gmail.com>
2025-07-23 14:55:59 -0300

    Alterando porte

--- app.py
+++ app.py
@@ -211,5 +211,5 @@ if __name__ == "__main__":
     thread = threading.Thread(target = agendador)
     thread.daemon = True
     thread.start()
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5005)
     # app.run(debug=True)
