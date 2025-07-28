41df2bf2f1ce6c440102cd05f963ca102501d54a
Gabreu <gabreumartins02@gmail.com>
2025-07-28 11:21:54 -0300

    Alterando alguns campor de widht de % para vw

--- app.py
+++ app.py
@@ -209,5 +209,5 @@ thread.start()
 
 print("Totalmente iniciado")
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5005, debug = True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005, debug = True)
