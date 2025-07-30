248379a57f10a8b2e044c167fcfb37c0cfcb454c
Gabreu <gabreumartins02@gmail.com>
2025-07-30 15:01:25 -0300

    a

--- app.py
+++ app.py
@@ -256,5 +256,5 @@ thread = threading.Thread(target=agendador)
 thread.daemon = True
 thread.start()
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5005, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005, debug=True)
